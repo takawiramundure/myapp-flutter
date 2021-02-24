1d101b5ce0f1e1d7c14ae970038258b9320e8a88
takawiramundure <takawiramundure@gmail.com>
2021-02-24 11:56:41 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text(
+              'questions[questionIndex]',
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
